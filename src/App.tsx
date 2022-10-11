86ff4bafed9e77de6d6e5665d1eacd2ccff83c31
brilliantwebdev1125 <handsomeprince1125@gmail.com>
2022-10-11 09:03:02 -0700

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { RootState } from "./redux/store";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">cgs</div>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/locale/en/index.js
@@ -0,0 +1,10 @@
+const en = {
+  translation: {
+    tabs: {},
+    input: {
+      placeholder: "",
+    },
+  },
+};
+
+export default en;
